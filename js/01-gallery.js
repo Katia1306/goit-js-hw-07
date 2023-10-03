81a9b3816eb90578c38986867f44f6f3e7ff1761
Katia1306 <kateryna.zhylan@gmail.com>
2023-10-03 21:09:48 +0200

    update1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const gallery = document.querySelector(".gallery");
+ 
+gallery.insertAdjacentHTML('afterbegin', createMarkup(gallery));
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original }) => ` 
+    <li>
+        
+       <a href="${preview}"></a>
+       <a href="${original}"></a>
+        
+    </li>
+    
+    ` ); join('')
+}
+
 console.log(galleryItems);
+
+<li>
+       
+      </li>
