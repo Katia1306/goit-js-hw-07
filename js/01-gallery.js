02c647b3f971285d0ada7db29c68065ef791c8e7
Katia1306 <kateryna.zhylan@gmail.com>
2023-10-03 23:09:26 +0200

    update2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const listEl = document.querySelector('.gallery');
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+}).join('');
+listEl.insertAdjacentHTML("beforeend", typset);
+const modalOpen = (event) => {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        console.log('NEpraça');
+        return;
+    }
+    const clickedImageAlt = event.target.getAttribute('alt');
+    const clickedImageSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(
+        `<img src='${clickedImageSrc}' alt='${clickedImageAlt}'/>`, {
+            onShow: (instance) => {
+                document.addEventListener('keydown', modalClose);
+            },
+            onClose: (instance) => {
+                document.removeEventListener('keydown', modalClose);
+                instance = null;
+            }
+        });
+    instance.show();
+};
+const modalClose = (event) => {
+    if (event.key !== 'Escape') {
+        return;
+    }
+    instance.close();
+};
+document.addEventListener('keydown', modalClose);
+listEl.addEventListener('click', (event) => {
+    modalOpen(event);
+});
 
-const gallery = document.querySelector(".gallery");
- 
-gallery.insertAdjacentHTML('afterbegin', createMarkup(gallery));
-
-function createMarkup(arr) {
-    return arr.map(({ preview, original }) => ` 
-    <li>
-        
-       <a href="${preview}"></a>
-       <a href="${original}"></a>
-        
-    </li>
-    
-    ` ); join('')
-}
-
-console.log(galleryItems);
-
-<li>
-       
-      </li>
